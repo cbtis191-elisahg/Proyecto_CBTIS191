fd128932ca1bf346825446e05585ca5d94dbcba4
elisahg <elisa.herrera@cbtis191.edu.mx>
2025-12-01 00:38:32 -0600

    PRIMERA CARGA

---
+++ ControlEntrada/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ControlentradaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ControlEntrada'
---
+++ Sys_CBTis191/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Sys_CBTis191 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sys_CBTis191.settings')
+
+application = get_asgi_application()
